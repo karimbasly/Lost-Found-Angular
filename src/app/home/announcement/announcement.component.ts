271eaac691e0824832f7586de57250045fd5f98f
karimbasly <karimbasli@live.fr>
2022-05-22 16:52:36 +0200

    Fix

---
+++ src/app/home/announcement/announcement.component.ts
@@ -0,0 +1,17 @@
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+
+
+@Component({
+  templateUrl: 'announcement.component.html',
+  styleUrls: ['announcement.component.css'],
+})
+export class AnnouncementComponent {
+  url:string="https://material.angular.io/assets/img/examples/shiba1.jpg";
+
+
+
+
+
+}
+
+
